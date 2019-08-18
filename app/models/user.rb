253f5f603ac59d7cdd24e8477b9dad654225f7e1
Mihai <40754142+Mihai23I@users.noreply.github.com>
2019-08-18 23:16:02 +0300

    Lic2_Users Add model user + sign_up page (#2)
    * Update dockerfile
    
    * User model + first migration
    
    * Add password
    
    * Fix phone and admin role
    
    * Sign_up users

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :city, length: { maximum: 50 }
+  validates :address, length: { maximum: 255 }
+  validates :phone, length: { is: 10 }, allow_nil: true, allow_blank: true, numericality: true
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#new'
+  get '/sign_up', to: 'users#new'
+  post '/sign_up', to: 'users#create'
+
+  resources :users
 end
