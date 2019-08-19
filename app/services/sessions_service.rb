5885c94119c0a802c58268c3ccd2dd6d9b79a69b
Mihai <40754142+Mihai23I@users.noreply.github.com>
2019-08-19 09:10:59 +0300

    Remember me (#4)

--- app/services/sessions_service.rb
+++ app/services/sessions_service.rb
@@ -1,15 +1,25 @@
 module SessionsService
-  def log_in(user)
+  def log_in(user, is_remembered = false)
     session[:user_id] = user.id
+    is_remembered == '1' ? remember(user) : forget(user)
   end
 
   def log_out
     session.delete(:user_id)
     @current_user = nil
+    forget(current_user)
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user&.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
@@ -18,5 +28,17 @@ module SessionsService
 
   def logged_in_as_admin?
     @current_user&.admin
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_18_162442) do
+ActiveRecord::Schema.define(version: 2019_08_19_052729) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_08_18_162442) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
