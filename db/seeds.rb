7f89355b809f970bdf915dee0a41778aadc4261c
Mihai23I <96mihai.ionescu@gmail.com>
2019-08-25 19:41:24 +0300

    Update db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,9 @@ User.create!(name: 'Example User',
                activated: true,
                activated_at: Time.zone.now)
 end
+
+cities = ["Adjud", "Aiud", "Alba Iulia", "Alexandria", "Arad", "Bacău", "Baia Mare", "Băilești", "Bârlad", "Beiuș", "Bistrița", "Blaj", "Botoșani", "Brad", "Brăila", "Brașov", "București", "Buzău", "Calafat", "Călărași", "Câmpia Turzii", "Câmpina", "Câmpulung", "Câmpulung Moldovenesc", "Caracal", "Caransebeș", "Carei", "Cluj Napoca", "Codlea", "Constanța", "Craiova", "Curtea de Argeș", "Dej", "Deva", "Dorohoi", "Drăgășani", "Drobeta-Turnu Severin", "Făgăraș", "Fălticeni", "Fetești", "Focșani", "Galați", "Gheorgheni", "Gherla", "Giurgiu", "Hunedoara", "Huși", "Iași", "Lugoj", "Lupeni", "Mangalia", "Marghita", "Medgidia", "Mediaș", "Miercurea Ciuc", "Moinești", "Moreni", "Motru", "Odorheiu Secuiesc", "Oltenița", "Onești", "Oradea", "Orăștie", "Orșova", "Pașcani", "Petroșani", "Piatra Neamț", "Pitești", "Ploiești", "Rădăuți", "Râmnicu Sărat", "Râmnicu Vâlcea", "Reghin", "Reșița", "Roman", "Roșiorii de Vede", "Săcele", "Salonta", "Satu Mare", "Sebeș", "Sfântu Gheorghe", "Sibiu", "Sighetu Marmației", "Sighișoara", "Slatina", "Slobozia", "Suceava", "Târgoviște", "Târgu Jiu", "Târgu Mureș", "Târgu Secuiesc", "Târnăveni", "Tecuci", "Timișoara", "Toplița", "Tulcea", "Turda", "Turnu Măgurele", "Urziceni", "Vaslui", "Vatra Dornei", "Vulcan", "Zalău"]
+
+cities.each do |city|
+  City.create(name: city, country: "Romania")
+end
