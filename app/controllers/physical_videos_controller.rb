c9b950b61c7bc34d442d4c6c0aefb7864d8b5e07
Mihai23i <96mihai.ionescu@gmail.com>
2019-08-27 22:20:45 +0300

    Update after mistake

--- app/controllers/physical_videos_controller.rb
+++ app/controllers/physical_videos_controller.rb
@@ -1,6 +1,6 @@
 class PhysicalVideosController < ApplicationController
   def index
-    @physical_videos = PhysicalVideo.where('physical_items.item_id = ?', params[:item_id]).page params[:page]
+    @physical_videos = PhysicalVideo.joins(:physical_item).where('physical_items.item_id = ?', params[:item_id]).page params[:page]
     @item = Item.find(params[:item_id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+cities = ["Adjud", "Aiud", "Alba Iulia", "Alexandria", "Arad", "Bacău", "Baia Mare", "Băilești", "Bârlad", "Beiuș", "Bistrița", "Blaj", "Botoșani", "Brad", "Brăila", "Brașov", "București", "Buzău", "Calafat", "Călărași", "Câmpia Turzii", "Câmpina", "Câmpulung", "Câmpulung Moldovenesc", "Caracal", "Caransebeș", "Carei", "Cluj Napoca", "Codlea", "Constanța", "Craiova", "Curtea de Argeș", "Dej", "Deva", "Dorohoi", "Drăgășani", "Drobeta-Turnu Severin", "Făgăraș", "Fălticeni", "Fetești", "Focșani", "Galați", "Gheorgheni", "Gherla", "Giurgiu", "Hunedoara", "Huși", "Iași", "Lugoj", "Lupeni", "Mangalia", "Marghita", "Medgidia", "Mediaș", "Miercurea Ciuc", "Moinești", "Moreni", "Motru", "Odorheiu Secuiesc", "Oltenița", "Onești", "Oradea", "Orăștie", "Orșova", "Pașcani", "Petroșani", "Piatra Neamț", "Pitești", "Ploiești", "Rădăuți", "Râmnicu Sărat", "Râmnicu Vâlcea", "Reghin", "Reșița", "Roman", "Roșiorii de Vede", "Săcele", "Salonta", "Satu Mare", "Sebeș", "Sfântu Gheorghe", "Sibiu", "Sighetu Marmației", "Sighișoara", "Slatina", "Slobozia", "Suceava", "Târgoviște", "Târgu Jiu", "Târgu Mureș", "Târgu Secuiesc", "Târnăveni", "Tecuci", "Timișoara", "Toplița", "Tulcea", "Turda", "Turnu Măgurele", "Urziceni", "Vaslui", "Vatra Dornei", "Vulcan", "Zalău"]
+
+cities.each do |city|
+  City.create(name: city, country: "Romania")
+end
+
 User.create!(name: 'Example User',
              email: 'admin@example.com',
              password: '123456',
              password_confirmation: '123456',
              admin: true,
              activated: true,
-             activated_at: Time.zone.now)
+             activated_at: Time.zone.now,
+             city_id: City.first.id)
 
 50.times do |n|
   name = Faker::Name.name
@@ -25,11 +33,17 @@ User.create!(name: 'Example User',
                password: password,
                password_confirmation: password,
                activated: true,
-               activated_at: Time.zone.now)
+               activated_at: Time.zone.now,
+               city_id: City.first.id)
 end
 
-cities = ["Adjud", "Aiud", "Alba Iulia", "Alexandria", "Arad", "Bacău", "Baia Mare", "Băilești", "Bârlad", "Beiuș", "Bistrița", "Blaj", "Botoșani", "Brad", "Brăila", "Brașov", "București", "Buzău", "Calafat", "Călărași", "Câmpia Turzii", "Câmpina", "Câmpulung", "Câmpulung Moldovenesc", "Caracal", "Caransebeș", "Carei", "Cluj Napoca", "Codlea", "Constanța", "Craiova", "Curtea de Argeș", "Dej", "Deva", "Dorohoi", "Drăgășani", "Drobeta-Turnu Severin", "Făgăraș", "Fălticeni", "Fetești", "Focșani", "Galați", "Gheorgheni", "Gherla", "Giurgiu", "Hunedoara", "Huși", "Iași", "Lugoj", "Lupeni", "Mangalia", "Marghita", "Medgidia", "Mediaș", "Miercurea Ciuc", "Moinești", "Moreni", "Motru", "Odorheiu Secuiesc", "Oltenița", "Onești", "Oradea", "Orăștie", "Orșova", "Pașcani", "Petroșani", "Piatra Neamț", "Pitești", "Ploiești", "Rădăuți", "Râmnicu Sărat", "Râmnicu Vâlcea", "Reghin", "Reșița", "Roman", "Roșiorii de Vede", "Săcele", "Salonta", "Satu Mare", "Sebeș", "Sfântu Gheorghe", "Sibiu", "Sighetu Marmației", "Sighișoara", "Slatina", "Slobozia", "Suceava", "Târgoviște", "Târgu Jiu", "Târgu Mureș", "Târgu Secuiesc", "Târnăveni", "Tecuci", "Timișoara", "Toplița", "Tulcea", "Turda", "Turnu Măgurele", "Urziceni", "Vaslui", "Vatra Dornei", "Vulcan", "Zalău"]
+t.string "name", null: false
+t.string "address", null: false
+t.string "schedule"
+t.datetime "created_at", null: false
+t.datetime "updated_at", null: false
+t.text "alternative_contact"
+t.integer "coordinate_id"
+t.integer "city_id"
 
-cities.each do |city|
-  City.create(name: city, country: "Romania")
-end
+Location.create!(name: "Biblioteca Națională a României", address: "	B-dul Unirii nr. 22, sector 3, București", city: City.find_by(name: "București"), schedule: "De la ora 8 la 20")
