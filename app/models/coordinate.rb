da1bf3a8156bde8ade6c3d6efedf2ece62f31b3d
Mihai23i <96mihai.ionescu@gmail.com>
2019-09-01 16:55:01 +0300

    Save distance :)

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Coordinate < ApplicationRecord
   validates :longitude, presence: true
   validates :latitude, presence: true
@@ -10,4 +12,19 @@ class Coordinate < ApplicationRecord
   end
 
   def self.calculate_distance(coordinates1, coordinates2)
+    earth_radius_in_kms = 6371
+    phi1 = Coordinate.degree_to_radian(coordinates1.latitude)
+    phi2 = Coordinate.degree_to_radian(coordinates2.latitude)
+    delta_phi = Coordinate.degree_to_radian(coordinates1.latitude - coordinates2.latitude)
+    delta_lambda = Coordinate.degree_to_radian(coordinates1.longitude - coordinates2.longitude)
+    a = Math.sin(delta_phi / 2)**2 +
+        Math.cos(phi1) * Math.cos(phi2) *
+        Math.sin(delta_lambda / 2)**2
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    distance_in_kms = earth_radius_in_kms * c
+  end
+
+  def self.degree_to_radian(degree)
+    degree * Math::PI / 180
+  end
 end
