729e1a195c52238ac88a616e34960493c3ac48ab
Mihai23i <96mihai.ionescu@gmail.com>
2019-09-07 15:16:24 +0300

    User administrator

--- app/controllers/administrator/users_controller.rb
+++ app/controllers/administrator/users_controller.rb
@@ -1,25 +1,21 @@
 class Administrator::UsersController < AdminsController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /administrator/users
   def index
-    @users = User.all
+    @q = User.ransack(params[:q])
+    @users = @q.result.order(updated_at: :desc).page(params[:page])
   end
 
-  # GET /administrator/users/1
   def show
   end
 
-  # GET /administrator/users/new
   def new
     @user = User.new
   end
 
-  # GET /administrator/users/1/edit
   def edit
   end
 
-  # POST /administrator/users
   def create
     @user = User.new(user_params)
 
@@ -30,7 +26,6 @@ class Administrator::UsersController < AdminsController
     end
   end
 
-  # PATCH/PUT /administrator/users/1
   def update
     if @user.update(user_params)
       redirect_to [:administrator, @user], notice: 'User was successfully updated.'
@@ -39,19 +34,16 @@ class Administrator::UsersController < AdminsController
     end
   end
 
-  # DELETE /administrator/users/1
   def destroy
     @user.destroy
     redirect_to administrator_users_url, notice: 'User was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def user_params
       params.require(:user).permit(:id, :client_id, :email, :admin, :city_id, :activated, :activated_at)
     end
